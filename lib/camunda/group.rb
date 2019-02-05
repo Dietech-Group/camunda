505ae4a896b634cfa35a811934d6bbdb5185a0d1
Daniel Schwab <schwab@infai.org>
2019-02-05 17:19:44 +0100

    Add class method to create and delete camunda group members.

--- lib/camunda/group.rb
+++ lib/camunda/group.rb
@@ -2,4 +2,14 @@ class Camunda::Group < Camunda::Api
   def self.collection_name
     'group'
   end
+
+  def self.member_create(id, user_id)
+    group = self.new(id: id)
+    group.put("members/#{user_id}", nil, nil)
+  end
+
+  def self.member_delete(id, user_id)
+    group = self.new(id: id)
+    group.delete("members/#{user_id}")
+  end
 end
