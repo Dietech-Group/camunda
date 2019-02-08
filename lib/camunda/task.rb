abed1db11585f30cea7269187a0e094a1982b58d
Daniel Schwab <schwab@infai.org>
2019-02-08 16:21:22 +0100

    Add add comment and list comment methods to task.

--- lib/camunda/task.rb
+++ lib/camunda/task.rb
@@ -2,4 +2,17 @@ class Camunda::Task < Camunda::Api
   def self.collection_name
     'task'
   end
+
+  def self.comment_create(id, comment)
+    task = self.new(id: id)
+    task.post("comment/create", nil,
+               {
+                   "message": comment
+               })
+  end
+
+  def self.comments(id)
+    task = self.new(id: id)
+    task.get("comment", nil)
+  end
 end
